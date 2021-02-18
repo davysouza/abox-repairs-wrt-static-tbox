c0512bc1a2b80134f2e454cba184e4a041b137bd
Francesco Kriegel <francesco-kriegel@users.noreply.github.com>
2021-02-18 08:31:33 +0100

    Crashing when an alleged repair type contains owl:Thing; fixed JavaDoc

--- RepairImplementation/src/main/java/de/tu_dresden/lat/abox_repairs/repair_types/RepairTypeHandler.java
+++ RepairImplementation/src/main/java/de/tu_dresden/lat/abox_repairs/repair_types/RepairTypeHandler.java
@@ -67,7 +67,7 @@ public class RepairTypeHandler {
     /**
      * Check whether the given repair pre-type is already premise-saturated
      * 
-     * @param repairPreType (a repair pre-type)
+     * @param repairPreType	a repair pre-type
      * @return
      * true if the given repair pre-type is already premise-saturated
      */
@@ -97,8 +97,8 @@ public class RepairTypeHandler {
      * Given a repair pre-type and a random object, this method will this repair pre-type
      * to a repair type that has been premise-saturated
      * 
-     * @param preType (a repair pre-type that may not be premise-saturated yet)
-     * @param random (a random object that help choose a random repair type that will be returned)
+     * @param preType 	a repair pre-type that may not be premise-saturated yet)
+     * @param random 	a random object that help choose a random repair type that will be returned)
      * @return
      * a random repair type
      */
@@ -204,9 +204,9 @@ public RepairType convertToRandomRepairType(Set<OWLClassExpression> preType, Ran
 
 	/**
 	 * 
-	 * @param type (a repair type)
+	 * @param type	a repair type
 	 * 
-	 * @param exp (a class expression)
+	 * @param exp 	a class expression
 	 * 
 	 * @return
 	 * the set that contains minimal repair pre-types that cover the union of 
@@ -229,8 +229,8 @@ public RepairType convertToRandomRepairType(Set<OWLClassExpression> preType, Ran
     
     /**
      * 
-     * @param type (a repair type)
-     * @param expSet (a set of class expressions)
+     * @param type		a repair type
+     * @param expSet 	a set of class expressions
      * @return
      * the set that contains minimal repair pre-types that cover the union of the two input sets
      */
