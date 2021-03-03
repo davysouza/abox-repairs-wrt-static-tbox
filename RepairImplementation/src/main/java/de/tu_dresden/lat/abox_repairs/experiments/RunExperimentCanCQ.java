85785247f128c7289d586b83f282b63278137f36
Francesco Kriegel <francesco-kriegel@users.noreply.github.com>
2021-03-03 19:18:02 +0100

    Using 64bit power-sets for attempting to compute canonical repairs

---
+++ RepairImplementation/src/main/java/de/tu_dresden/lat/abox_repairs/experiments/RunExperimentCanCQ.java
@@ -0,0 +1,82 @@
+package de.tu_dresden.lat.abox_repairs.experiments;
+
+import de.tu_dresden.lat.abox_repairs.generator.CanonicalRepairGenerator;
+import de.tu_dresden.lat.abox_repairs.generator.RepairGenerator;
+import de.tu_dresden.lat.abox_repairs.ontology_tools.CycleChecker;
+import de.tu_dresden.lat.abox_repairs.ontology_tools.OntologyPreparations;
+import de.tu_dresden.lat.abox_repairs.reasoning.ReasonerFacade;
+import de.tu_dresden.lat.abox_repairs.saturation.ABoxSaturator;
+import de.tu_dresden.lat.abox_repairs.saturation.ChaseGenerator;
+import de.tu_dresden.lat.abox_repairs.saturation.SaturationException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.parameters.Imports;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RunExperimentCanCQ {
+    private static Logger logger = LogManager.getLogger(RunExperimentCanCQ.class);
+
+    private RunExperimentCanCQ() {
+    }
+
+    public static void main(String[] args) throws OWLOntologyCreationException, SaturationException {
+        final String ontologyFileName = args[0];
+        final OWLOntology ontology =
+                OWLManager.createOWLOntologyManager()
+                        .loadOntologyFromOntologyDocument(new File(ontologyFileName));
+        OntologyPreparations.prepare(ontology);
+
+        long startTime = System.nanoTime();
+
+        int oldIndividuals = ontology.getIndividualsInSignature().size();
+        long oldAssertions = ontology.aboxAxioms(Imports.INCLUDED).count();
+
+        long start = System.nanoTime();
+        logger.info("Init reasoner facade without TBox");
+        final List<OWLClassExpression> additionalExpressions = new LinkedList<>();
+        additionalExpressions.addAll(ontology.getNestedClassExpressions());
+        final ReasonerFacade reasonerWithoutTBox = ReasonerFacade.newReasonerFacadeWithoutTBox(additionalExpressions, ontology.getOWLOntologyManager());
+        logger.info("Init reasoner facade with TBox");
+        final ReasonerFacade reasonerWithTBox = ReasonerFacade.newReasonerFacadeWithTBox(ontology, additionalExpressions);
+        logger.info("Initialising reasoner facades took " + ((double) System.nanoTime() - start) / 1_000_000_000);
+
+        CycleChecker cycleChecker = new CycleChecker(reasonerWithTBox);
+        if (cycleChecker.cyclic()) {
+            System.out.println("Cyclic!");
+            System.exit(0);
+        }
+
+        System.out.println("\nCQ-saturation");
+        final ABoxSaturator saturator = new ChaseGenerator();
+        saturator.saturate(ontology);
+
+        reasonerWithTBox.update();
+
+        System.out.println("\nCQ-repair");
+        final RepairGenerator repairGenerator = new CanonicalRepairGenerator(ontology, null);
+        repairGenerator.setReasoner(reasonerWithTBox, reasonerWithoutTBox);
+        repairGenerator.repair();
+        /*ontology = repairGenerator.getRepair();*/
+
+        double timeRepairing = (double) (System.nanoTime() - startTime) / 1_000_000_000;
+
+        /**
+         * Please do not remove! This is not debugging output, but used for the evaluation of the experiments.
+         */
+        System.out.print("#Individuals (orig/sat/repair): "
+                + oldIndividuals + "/"
+                + (oldIndividuals + saturator.addedIndividuals()) + "/" +
+                repairGenerator.getNumberOfCollectedIndividuals());
+        System.out.print(" #Assertions (orig/sat/repair): "
+                + oldAssertions + "/"
+                + (oldAssertions + saturator.addedAssertions()) + "/-");
+        System.out.println(" Duration (sat/repair sec.): " + saturator.getDuration() + "/" + timeRepairing);
+    }
+}
--- RepairImplementation/src/main/java/de/tu_dresden/lat/abox_repairs/generator/CanonicalRepairGenerator.java
+++ RepairImplementation/src/main/java/de/tu_dresden/lat/abox_repairs/generator/CanonicalRepairGenerator.java
@@ -1,71 +1,85 @@
 package de.tu_dresden.lat.abox_repairs.generator;
 
+import com.google.common.collect.Sets;
+import de.tu_dresden.lat.abox_repairs.repair_type.RepairType;
+import fr.pixelprose.count.generator.PowerSet;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.semanticweb.owlapi.model.*;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
-import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom;
-import org.semanticweb.owlapi.model.OWLOntology;
-
-import com.google.common.collect.Sets; 
-
-import de.tu_dresden.lat.abox_repairs.repair_type.RepairType;
-
 /**
  * Careful: this does not compute the canonical repair, but only the variables that would be needed by it.
  */
 public class CanonicalRepairGenerator extends RepairGenerator {
-	
-	private static Logger logger = LogManager.getLogger(CanonicalRepairGenerator.class);
-	
-	public CanonicalRepairGenerator(OWLOntology inputOntology,
-			Map<OWLNamedIndividual, RepairType> inputSeedFunction) {
-		//super(inputOntology, inputSeedFunction);
-		super(inputOntology, Collections.emptyMap());
 
-		setOfCollectedIndividuals = new HashSet<>();
-	}
-	
+    private static Logger logger = LogManager.getLogger(CanonicalRepairGenerator.class);
+    private final Set<Pair<OWLIndividual, RepairType>> individualsInTheRepair = new HashSet<>();
 
-	@Override
-	protected void generateVariables() {
-		
-		for(OWLNamedIndividual individual : ontology.getIndividualsInSignature()) {
-			Set<OWLClassExpression> setOfAtoms = new HashSet<>();
-			for(OWLClassExpression assertedConcept : reasonerWithTBox.instanceOf(individual)) {
-				if(atomForRepairType(assertedConcept)) {
-					setOfAtoms.add(assertedConcept);
-				}
-			}
-			Set<Set<OWLClassExpression>> powerSet = Sets.powerSet(setOfAtoms);
+    public CanonicalRepairGenerator(OWLOntology inputOntology,
+                                    Map<OWLNamedIndividual, RepairType> inputSeedFunction) {
+        //super(inputOntology, inputSeedFunction);
+        super(inputOntology, Collections.emptyMap());
+
+        setOfCollectedIndividuals = new HashSet<>();
+    }
+
+    @Override
+    public int getNumberOfCollectedIndividuals() {
+        return individualsInTheRepair.size();
+    }
+
+    @Override
+    protected void generateVariables() {
+
+        for (OWLNamedIndividual individual : ontology.getIndividualsInSignature()) {
+            Set<OWLClassExpression> setOfAtoms = new HashSet<>();
+            for (OWLClassExpression assertedConcept : reasonerWithTBox.instanceOf(individual)) {
+                if (atomForRepairType(assertedConcept)) {
+                    setOfAtoms.add(assertedConcept);
+                }
+            }
+			/*Set<Set<OWLClassExpression>> powerSet = Sets.powerSet(setOfAtoms);
 			for(Set<OWLClassExpression> repairTypeCandidate: powerSet) {
 				if(typeHandler.isPremiseSaturated(repairTypeCandidate, individual)) {
 					RepairType type = typeHandler.newMinimisedRepairType(repairTypeCandidate);
 					createCopy(individual, type);
 				}
-			}
-		}
-	}
+			}*/
+            final Iterable<Iterable<OWLClassExpression>> powerSet = new PowerSet(setOfAtoms, 0, setOfAtoms.size());
+            for (Iterable<OWLClassExpression> repairTypeCandidate : powerSet) {
+                final Set<OWLClassExpression> _repairTypeCandidate = Sets.newHashSet(repairTypeCandidate);
+                if (typeHandler.isPremiseSaturated(_repairTypeCandidate, individual)) {
+                    RepairType type = typeHandler.newMinimisedRepairType(_repairTypeCandidate);
+                    /*createCopy(individual, type);*/
+                    individualsInTheRepair.add(Pair.of(individual, type));
+                }
+            }
+        }
+    }
 
-	private boolean atomForRepairType(OWLClassExpression exp){
-		/* As you want to test whether 'exp' is an atom, you must replace the condition
-		*  !reasonerWithTBox.equivalentToOWLThing(exp)
-		*  with !reasonerWithoutTBox.equivalentToOWLThing(exp) */
-		return (exp instanceof OWLClass || exp instanceof OWLObjectSomeValuesFrom)
-				&& !reasonerWithoutTBox.equivalentToOWLThing(exp);
-	}
-
-	
+    private boolean atomForRepairType(OWLClassExpression exp) {
+        /* As you want to test whether 'exp' is an atom, you must replace the condition
+         *  !reasonerWithTBox.equivalentToOWLThing(exp)
+         *  with !reasonerWithoutTBox.equivalentToOWLThing(exp) */
+        return (exp instanceof OWLClass || exp instanceof OWLObjectSomeValuesFrom)
+                && !reasonerWithoutTBox.equivalentToOWLThing(exp);
+    }
 
 
-	@Override
-	protected void initialise() {
-		
-	}
+    @Override
+    protected void initialise() {
+        // Do nothing.
+    }
+
+
+    @Override
+    protected void generateMatrix() throws OWLOntologyCreationException {
+        // Do nothing.
+    }
 }
